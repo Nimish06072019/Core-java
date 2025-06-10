75b91acf5ccb126f2763e260ddbcc24715a5dea0
Nimish_12 <82524713+Nimish06072019@users.noreply.github.com>
2025-06-10 23:17:40 +0530

    More on Inheritance

--- Inhertiance/src/MultiLevelTest.java
+++
@@ -1,9 +0,0 @@
-
-public class MultiLevelTest {
-
-	public static void main(String[] args) {
-		Child c1 = new Child();
-		c1.f1();
-		c1.f2();
-	}
-}
--- Inhertiance/src/com/hierarchical/Inheritance/Bus.java
+++
@@ -1,5 +0,0 @@
-package com.hierarchical.Inheritance;
-
-public class Bus extends Vehicle {
-	
-}
